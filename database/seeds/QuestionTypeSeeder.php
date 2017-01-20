3de51ab71b015e50d23babdc7acebfe9e2bfbcc9
Daniel Luna <dluna@aper.net>
2017-01-19 18:17:26 -0600

    New Button options added

--- database/seeds/QuestionTypeSeeder.php
+++ database/seeds/QuestionTypeSeeder.php
@@ -23,27 +23,41 @@ class QuestionTypeSeeder extends Seeder{
         
         DB::table('cat_QuestionType')->insert([
             'id'            => 2,
-            'name'          => "Opción Múltiple",
-            'icon'         => "glyphicon glyphicon-list",
+            'name'          => "Opción Múltiple / Una respuesta",
+            'icon'         => "glyphicon glyphicon-record",
             'created_by'    => 1
         ]);
         
         DB::table('cat_QuestionType')->insert([
             'id'            => 3,
+            'name'          => "Opción Múltiple / Varias Respuestas",
+            'icon'         => "glyphicon glyphicon-check",
+            'created_by'    => 1
+        ]);
+        
+        DB::table('cat_QuestionType')->insert([
+            'id'            => 4,
+            'name'          => "Pregunta de satisfacción",
+            'icon'         => "glyphicon glyphicon-heart",
+            'created_by'    => 1
+        ]);
+        
+        DB::table('cat_QuestionType')->insert([
+            'id'            => 5,
             'name'          => "Puntuación",
             'icon'          => "glyphicon glyphicon-star-empty",
             'created_by'    => 1
         ]);
         
         DB::table('cat_QuestionType')->insert([
-            'id'            => 4,
-            'name'          => "Texto informativo",
+            'id'            => 6,
+            'name'          => "Texto simple",
             'icon'          => "glyphicon glyphicon-font",
             'created_by'    => 1
         ]);
         
         DB::table('cat_QuestionType')->insert([
-            'id'            => 5,
+            'id'            => 7,
             'name'          => "Si / NO",
             'icon'          => "glyphicon glyphicon-thumbs-up",
             'created_by'    => 1
