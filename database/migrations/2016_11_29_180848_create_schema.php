0e0adb19cd11fa10b3502f7009b423c1156150db
Daniel Luna <dluna@aper.net>
2017-03-22 14:42:57 -0600

    answer column added

--- database/migrations/2016_11_29_180848_create_schema.php
+++ database/migrations/2016_11_29_180848_create_schema.php
@@ -118,7 +118,7 @@ class CreateSchema extends Migration {
             $table->integer('idSurvey')                 ->unsigned()->nullable(false);
             $table->integer('idQuestionType')           ->unsigned()->nullable(false);
             $table->integer('idParent')                 ->nullable(false)->default(0);
-            $table->string('conditionalAnswer')         ->nullable(false)->default(null);
+            $table->string('answer')         ->nullable(false)->default(null);
             $table->timestamp('created_at')             ->nullable(false);
             $table->integer('created_by')               ->nullable(false);
             $table->integer('updated_by')               ->nullable();
